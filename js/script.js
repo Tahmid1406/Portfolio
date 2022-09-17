0c8e19b17bb0790bcdb664b6af3312bbba35f496
tahmid1406 <tahmidpranto1406@gmail.com>
2022-09-18 04:20:10 +0600

    index page improved

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
 var typed = new Typed(".auto-input", {
-    strings : ["Machine Learnig", "Distributed Learning", "Blockchain"],
+    strings : ["Machine Learning", "Distributed Learning", "Blockchain"],
     typeSpeed: 85,
     backSpeed: 100,
     loop:true
